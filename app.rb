6e83cc5601d821e1591a6d0b6203a9abb64ed6a0
Abid Velshi <abid.velshi@Eugenes-iMac.local>
2015-01-29 18:20:10 -0500

    more changes

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'builder'
 
+class TwilioApp < Sinatra::Base
 post '/' do 
   builder do |xml|
-    xml.instruct!
-    xml.Response do
-      xml.Say("Alex,,,you should listen to Abid more often. He knows best.")
-    end
+      xml.instruct!
+        xml.Response do
+          xml.Say("Alex,,,you should listen to Abid more often. He knows best.")
+        end
+      end
     end
   end
 end
