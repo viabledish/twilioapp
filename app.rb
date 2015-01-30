aec859d60dc0327e3d078d772c5278e0e9c8c3e2
Abid Velshi <abid.velshi@Eugenes-iMac.local>
2015-01-30 12:15:47 -0500

    removed call post

--- app.rb
+++ app.rb
@@ -8,15 +8,3 @@ get '/hello' do
 </Response>
 EOF
 end
-
-post '/call' do
-# Get your Account Sid and Auth Token from twilio.com/user/account
-account_sid = 'AC0fc39d43ebae37ec0145c8f1a555edb2'
-auth_token = 'e2b92697a6808211e2f0bfca3bc56d67'
-@client = Twilio::REST::Client.new account_sid, auth_token
- 
-call = @client.account.calls.create(:url => "http://demo.twilio.com/docs/voice.xml",
-    :to => "+14168271871",
-    :from => "+14163615974")
-puts call.to
-end
